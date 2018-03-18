a2b675981b5cab8390d3802ddfa8dafc8560123d
phillipjbeasley <phillipjbeasley@gmail.com>
2018-03-18 17:23:04 -0400

    CRUD - READ
    - Add handler for a GET request
    - Add index.html file to be served

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html');
+	// Note: __dirname is directory that contains the JavaScript source code.
+	console.log(__dirname);
+})
+
+
 app.listen(3000, function() {
 	console.log('listening on 3000');
 })
