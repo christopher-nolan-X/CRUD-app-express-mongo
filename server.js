e76b955daf5d5ff2588933395bbf443a5bda864c
phillipjbeasley <phillipjbeasley@gmail.com>
2018-03-18 19:42:36 -0400

    MongoDB
    - install mongodb node module
    - connecto to database
    - hide config variables
    - POSTed to database collection, otherwise returned error

--- server.js
+++ server.js
@@ -1,22 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
+
+require('env2')('.env'); // loads all entries into process.env
 
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 
+MongoClient.connect(process.env.DB_URL, (err, client) => {
+	if (err) return console.log(err);
+	db = client.db('quote-machine');
+	app.listen(3000, () => {
+		console.log('listening on 3000');
+	})
+})
+
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 	// Note: __dirname is directory that contains the JavaScript source code.
-	console.log(__dirname);
+	// console.log(__dirname);
 })
+
+let db;
+
 
 app.post('/quotes', (req, res) => {
-	console.log(req.body);
-})
+	db.collection('quotes').save(req.body, (err, result) => {
+		if (err) return console.log(err);
 
-
-app.listen(3000, function() {
-	console.log('listening on 3000');
+		console.log('saved to database');
+		res.redirect('/');
+	})
 })
